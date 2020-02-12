f158ae06935985fff0a2cf5ded96703b33f454b7
nakano yosuke <naninuneno0411@gmail.com>
2020-02-13 01:33:50 +0900

    sample?

--- server.ts
+++ server.ts
@@ -1,6 +1,33 @@
-import http = require('http');
-var port = process.env.port || 1337
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+const vrmLoaderScript = document.createElement('script');
+vrmLoaderScript.src = 'https://cdn.jsdelivr.net/npm/babylon-vrm-loader/dist/index.js';
+document.head.appendChild(vrmLoaderScript);
+
+var createScene = function () {
+
+    // This creates a basic Babylon Scene object (non-mesh)
+    var scene = new BABYLON.Scene(engine);
+
+    // This creates and positions a free camera (non-mesh)
+    var camera = new BABYLON.ArcRotateCamera("camera1", -Math.PI / 2, Math.PI / 2, 2, new BABYLON.Vector3(0, 1.3, 0), scene);
+
+    // This targets the camera to scene origin
+    // camera.setTarget(new BABYLON.Vector3(0, 1.5, 0));
+
+    // This attaches the camera to the canvas
+    camera.attachControl(canvas, true);
+
+    // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
+    var light = new BABYLON.DirectionalLight("light", new BABYLON.Vector3(0.3, -1, 0.3), scene);
+
+    // Our built-in 'ground' shape.
+    var ground = BABYLON.MeshBuilder.CreateGround("ground", { width: 6, height: 6 }, scene);
+
+    vrmLoaderScript.onload = () => {
+        BABYLON.SceneLoader.Append('https://raw.githubusercontent.com/vrm-c/UniVRM/master/Tests/Models/Alicia_vrm-0.51/', 'AliciaSolid_vrm-0.51.vrm', scene, () => {
+
+        });
+    };
+
+    return scene;
+
+};
