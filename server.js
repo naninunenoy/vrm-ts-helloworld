e639bd5c64a16bcb4ca42c9cf25a012c1bf7431f
nakano yosuke <naninuneno0411@gmail.com>
2020-02-13 02:04:48 +0900

    src/test/index.ts

--- server.js
+++ server.js
@@ -1,9 +1,57 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const http = require("http");
-var port = process.env.port || 1337;
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+const BABYLON = require("@babylonjs/core");
+const engine_1 = require("@babylonjs/core/Engines/engine");
+const vrmLoaderScript = document.createElement('script');
+vrmLoaderScript.src = 'https://cdn.jsdelivr.net/npm/babylon-vrm-loader/dist/index.js';
+document.head.appendChild(vrmLoaderScript);
+var createScene = function () {
+    const debugProperties = getDebugProperties();
+    const canvas = document.getElementById('main-canvas');
+    const engine = new engine_1.Engine(canvas, true, {
+        alpha: false,
+        disableWebGL2Support: debugProperties.webgl1,
+    });
+    // This creates a basic Babylon Scene object (non-mesh)
+    var scene = new BABYLON.Scene(engine);
+    // This creates and positions a free camera (non-mesh)
+    var camera = new BABYLON.ArcRotateCamera("camera1", -Math.PI / 2, Math.PI / 2, 2, new BABYLON.Vector3(0, 1.3, 0), scene);
+    // This targets the camera to scene origin
+    // camera.setTarget(new BABYLON.Vector3(0, 1.5, 0));
+    // This attaches the camera to the canvas
+    camera.attachControl(canvas, true);
+    // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
+    var light = new BABYLON.DirectionalLight("light", new BABYLON.Vector3(0.3, -1, 0.3), scene);
+    // Our built-in 'ground' shape.
+    var ground = BABYLON.MeshBuilder.CreateGround("ground", { width: 6, height: 6 }, scene);
+    vrmLoaderScript.onload = () => {
+        BABYLON.SceneLoader.Append('https://raw.githubusercontent.com/vrm-c/UniVRM/master/Tests/Models/Alicia_vrm-0.51/', 'AliciaSolid_vrm-0.51.vrm', scene, () => {
+        });
+    };
+    return scene;
+};
+function getDebugProperties() {
+    const href = window.location.href;
+    return {
+        webgl1: href.includes('webgl1'),
+        shadow: href.includes('shadow'),
+        inspector: href.includes('inspector'),
+    };
+}
+class Main {
+    constructor() {
+        // http�T�[�o�[��ݒ肷��
+        const server = http.createServer((request, response) => this.requestHandler(request, response));
+        // �T�[�o�[���N�����ă��N�G�X�g��҂��󂯏�Ԃɂ���
+        server.listen('1337');
+    }
+    /*
+    * �T�[�o�[�Ƀ��N�G�X�g�����������Ɏ��s�����֐�
+    */
+    requestHandler(request, response) {
+        response.end('Hello! Node.js with TypeScript');
+    }
+}
+const main = new Main();
 //# sourceMappingURL=server.js.map
--- server.ts
+++ server.ts
@@ -1,33 +1,132 @@
-const vrmLoaderScript = document.createElement('script');
-vrmLoaderScript.src = 'https://cdn.jsdelivr.net/npm/babylon-vrm-loader/dist/index.js';
-document.head.appendChild(vrmLoaderScript);
+import { ArcRotateCamera } from '@babylonjs/core/Cameras/arcRotateCamera';
+import { Engine } from '@babylonjs/core/Engines/engine';
+import { DirectionalLight } from '@babylonjs/core/Lights/directionalLight';
+import { HemisphericLight } from '@babylonjs/core/Lights/hemisphericLight';
+import { PointLight } from '@babylonjs/core/Lights/pointLight';
+import { ShadowGenerator } from '@babylonjs/core/Lights/Shadows/shadowGenerator';
+import { SceneLoader } from '@babylonjs/core/Loading/sceneLoader';
+import { Vector3 } from '@babylonjs/core/Maths/math';
+import { Mesh } from '@babylonjs/core/Meshes/mesh';
+import { Scene } from '@babylonjs/core/scene';
+import { VRMManager } from '../vrm-manager';
 
-var createScene = function () {
+import '@babylonjs/core/Helpers/sceneHelpers';
+import '@babylonjs/core/Meshes/Builders/sphereBuilder';
+import '@babylonjs/core/Meshes/Builders/torusKnotBuilder';
+import '@babylonjs/inspector';
+import '../index';
 
-    // This creates a basic Babylon Scene object (non-mesh)
-    var scene = new BABYLON.Scene(engine);
+async function main() {
+    const debugProperties = getDebugProperties();
+    const canvas = document.getElementById('main-canvas') as HTMLCanvasElement;
+    const engine = new Engine(
+        canvas,
+        true,
+        {
+            alpha: false,
+            disableWebGL2Support: debugProperties.webgl1,
+        },
+    );
 
-    // This creates and positions a free camera (non-mesh)
-    var camera = new BABYLON.ArcRotateCamera("camera1", -Math.PI / 2, Math.PI / 2, 2, new BABYLON.Vector3(0, 1.3, 0), scene);
-
-    // This targets the camera to scene origin
-    // camera.setTarget(new BABYLON.Vector3(0, 1.5, 0));
-
-    // This attaches the camera to the canvas
+    const scene = new Scene(engine);
+    const camera = new ArcRotateCamera('MainCamera1', 0, 0, 3, new Vector3(0, 1.2, 0), scene, true);
+    camera.lowerRadiusLimit = 0.1;
+    camera.upperRadiusLimit = 20;
+    camera.wheelDeltaPercentage = 0.01;
+    camera.minZ = 0.3;
+    camera.position = new Vector3(0, 1.2, -3);
     camera.attachControl(canvas, true);
 
-    // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-    var light = new BABYLON.DirectionalLight("light", new BABYLON.Vector3(0.3, -1, 0.3), scene);
+    scene.createDefaultEnvironment({
+        createGround: true,
+        createSkybox: false,
+        enableGroundMirror: false,
+        enableGroundShadow: false,
+    });
 
-    // Our built-in 'ground' shape.
-    var ground = BABYLON.MeshBuilder.CreateGround("ground", { width: 6, height: 6 }, scene);
+    // Lights
+    const directionalLight = new DirectionalLight('DirectionalLight1', new Vector3(0, -0.5, 1.0), scene);
+    directionalLight.position = new Vector3(0, 25, -50);
+    directionalLight.setEnabled(true);
+    const hemisphericLight = new HemisphericLight('HemisphericLight1', new Vector3(-0.2, -0.8, -1), scene);
+    hemisphericLight.setEnabled(false);
+    const pointLight = new PointLight('PointLight1', new Vector3(0, 0, 1), scene);
+    pointLight.setEnabled(false);
 
-    vrmLoaderScript.onload = () => {
-        BABYLON.SceneLoader.Append('https://raw.githubusercontent.com/vrm-c/UniVRM/master/Tests/Models/Alicia_vrm-0.51/', 'AliciaSolid_vrm-0.51.vrm', scene, () => {
+    // Meshes
+    const standardMaterialSphere = Mesh.CreateSphere('StandardMaterialSphere1', 16, 1, scene);
+    standardMaterialSphere.position = new Vector3(1.5, 1.2, 0);
+    standardMaterialSphere.receiveShadows = true;
 
+    const shadowCaster = Mesh.CreateTorusKnot('ShadowCaster', 1, 0.2, 32, 32, 2, 3, scene);
+    shadowCaster.position = new Vector3(0.0, 5.0, -10.0);
+    shadowCaster.setEnabled(debugProperties.shadow);
+    if (debugProperties.shadow) {
+        const shadowGenerator = new ShadowGenerator(1024, directionalLight);
+        shadowGenerator.addShadowCaster(shadowCaster);
+    }
+
+    if (debugProperties.inspector) {
+        await scene.debugLayer.show({
+            globalRoot: document.getElementById('wrapper') as HTMLElement,
         });
+    }
+
+    // Expose current scene
+    (window as any).currentScene = scene;
+
+    scene.onBeforeRenderObservable.add(() => {
+        // SpringBone
+        if (!scene.metadata || !scene.metadata.vrmManagers) {
+            return;
+        }
+        const managers = scene.metadata.vrmManagers as VRMManager[];
+        const deltaTime = scene.getEngine().getDeltaTime();
+        managers.forEach((manager) => {
+            manager.update(deltaTime);
+        });
+    });
+    engine.runRenderLoop(() => {
+        scene.render();
+        shadowCaster.rotate(Vector3.Up(), 0.01);
+    });
+    window.addEventListener('resize', () => {
+        engine.resize();
+    });
+    await SceneLoader.AppendAsync('./', 'AliciaSolid.vrm', scene);
+
+    let fileCount = 1;
+    (document.getElementById('file-input') as HTMLInputElement).addEventListener('change', (evt) => {
+        const file = (evt as any).target.files[0];
+        console.log(`loads ${file.name} ${file.size} bytes`);
+        const currentMeshCount = scene.meshes.length;
+        SceneLoader.Append('file:', file, scene, () => {
+            console.log(`loaded ${file.name}`);
+            for (let i = currentMeshCount; i < scene.meshes.length; i++) {
+                scene.meshes[i].translate(Vector3.Right(), 1.5 * fileCount);
+                scene.meshes[i].receiveShadows = true;
+            }
+            fileCount++;
+        });
+    });
+}
+
+interface DebugProperties {
+    webgl1: boolean;
+    shadow: boolean;
+    inspector: boolean;
+}
+
+function getDebugProperties(): DebugProperties {
+    const href = window.location.href;
+
+    return {
+        webgl1: href.includes('webgl1'),
+        shadow: href.includes('shadow'),
+        inspector: href.includes('inspector'),
     };
+}
 
-    return scene;
-
-};
+main().catch((reason) => {
+    console.error(reason);
+});
